9281db0fc8a625042386df188c5b34075bb79d6b
Pranav Agarkar <90404176+PranavAgarkar07@users.noreply.github.com>
2026-01-15 23:57:29 +0530

    Time: 3 ms (32.29%), Space: 11.9 MB (14.64%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -7,15 +7,12 @@ public:
         for(int i=1;i<=n;i++){
             if(i%15==0){
                 result.push_back("FizzBuzz");
-                continue;
             }
             else if(i%5==0){
                 result.push_back("Buzz");
-                continue;
             }
             else if(i%3==0){
                 result.push_back("Fizz");
-                continue;
             }
             else{
                 result.push_back(to_string(i));
