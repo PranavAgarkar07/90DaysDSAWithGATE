3f5916aeaaa1824b6cc5a0cd43b9f0c4087b6063
Pranav Agarkar <90404176+PranavAgarkar07@users.noreply.github.com>
2026-01-15 23:56:57 +0530

    Time: 4 ms (3.32%), Space: 11.7 MB (41.57%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -5,7 +5,7 @@ public:
         vector<string> result;
 
         for(int i=1;i<=n;i++){
-            if(i%3==0 && i%5==0){
+            if(i%15==0){
                 result.push_back("FizzBuzz");
                 continue;
             }
