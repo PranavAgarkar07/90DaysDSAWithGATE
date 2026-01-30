0896a7333336f564616ad26ad85da2bfb23f9c0a
Pranav Agarkar <pranavagarkar8@gmail.com>
2026-01-31 00:17:58 +0530

    calculated time complexity

--- TowerOfHanoi.cpp
+++ TowerOfHanoi.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int i=0;
 void toh(int,char,char,char);
 int main() {
-  toh(10,'A','B','C');
+  toh(3,'A','B','C');
   cout<<endl;
   cout<<i<<endl;
   return 0;
@@ -13,8 +13,8 @@ void toh(int n, char x, char y,char z){
   i++;
   if(n==1) cout<<x<<"->"<<y<<"->";
   else{
-    toh(n-1,'A','C','B');
+    toh(n-1,x,z,y);
     cout<<x<<"->"<<y<<"->";
-    toh(n-1,'C','B','A');
+    toh(n-1,z,y,x);
   }
 }
